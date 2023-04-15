f30787e8196f0db4eba57335a0cb9027a6ab39eb
Hoang Binh <hbinhit221@gmail.com>
2023-04-15 14:32:36 +0700

    Add specification category field

--- DATH/frontend/src/app/routes/admin-management/specification-list/partials/specification-drawer/specification-drawer.component.ts
+++ DATH/frontend/src/app/routes/admin-management/specification-list/partials/specification-drawer/specification-drawer.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { NzMessageService } from 'ng-zorro-antd/message';
+import { SpecificationCategory } from 'src/app/models/specificationCategory.model';
 import { DrawerFormBaseComponent } from 'src/app/routes/components/drawer-form-base/drawer-form-base.component';
+import { SpecificationCategoryService } from 'src/app/services/specification-category.service';
 import { SpecificationService } from 'src/app/services/specification.service';
 import { checkResponseStatus } from 'src/app/shared/helper';
 
@@ -11,19 +13,35 @@ import { checkResponseStatus } from 'src/app/shared/helper';
   styleUrls: ['./specification-drawer.component.less']
 })
 export class SpecificationDrawerComponent extends DrawerFormBaseComponent implements OnInit {
+  categories: SpecificationCategory[] = [];
   constructor(
     protected override fb: FormBuilder,
     protected override cdr: ChangeDetectorRef,
     protected override message: NzMessageService,
-    protected specificationService: SpecificationService
+    protected specificationService: SpecificationService,
+    protected specificationCategoryService: SpecificationCategoryService,
   ){
     super(fb, cdr, message);
   }
+  override ngOnInit(): void {
+    this.initForm();
+    this.fetchCategories();
+  }
+
+  fetchCategories(){
+    this.specificationCategoryService.getAll().subscribe(res => {
+      if(checkResponseStatus(res)){
+        this.categories =res.data;
+      }
+    })
+  }
+
   override initForm(): void {
     this.drawerForm = this.fb.group({
       id: [null],
       code: [null, Validators.required],
       value: [null, Validators.required],
+      specificationCategoryId: [null, Validators.required],
       description: [null],
     })
   }
