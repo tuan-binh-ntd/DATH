404130188364fe192e6d6437bb630383a16c633a
Hoang Binh <hbinhit221@gmail.com>
2023-04-17 23:53:02 +0700

    Add UI for login, sign up customer

--- DATH/frontend/src/app/routes/customer-management/customer-header/customer-header.component.ts
+++ DATH/frontend/src/app/routes/customer-management/customer-header/customer-header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd/message';
 import { Customer } from 'src/app/models/customer.model';
 
 @Component({
@@ -7,8 +9,37 @@ import { Customer } from 'src/app/models/customer.model';
   styleUrls: ['./customer-header.component.less']
 })
 export class CustomerHeaderComponent {
+  constructor(private msg: NzMessageService,
+    private fb: FormBuilder){}
+  isVisible: boolean = false;
+  form!: FormGroup;
+
   customer!: Customer;
   ngAfterViewInit(){
     this.customer = JSON.parse(localStorage.getItem('user')!);
   }
+
+  initForm(){
+    this.form = this.fb.group({
+      id: [null],
+      code: [null, Validators.required],
+      value: [null, Validators.required],
+      specificationCategoryId: [null, Validators.required],
+      description: [null],
+    })
+  }
+
+  signIn(){
+    this.isVisible = true;
+  }
+
+  signUp(){
+    this.isVisible = true;
+  }
+
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+
 }
