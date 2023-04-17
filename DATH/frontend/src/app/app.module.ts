5d2774d66a56d0edc93d34a165794bc42f8bd962
Hoang Binh <hbinhit221@gmail.com>
2023-04-17 23:54:49 +0700

    Change language to EN

--- DATH/frontend/src/app/app.module.ts
+++ DATH/frontend/src/app/app.module.ts
@@ -5,15 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { DefaultInterceptor } from './interceptors/default.interceptor';
 import { CookieService } from 'ngx-cookie-service';
-
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+registerLocaleData(en);
 
 
 @NgModule({
@@ -28,6 +28,7 @@ import { CookieService } from 'ngx-cookie-service';
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: DefaultInterceptor, multi: true },
+    { provide: NZ_I18N, useValue: en_US },
     [CookieService],
   ],
   bootstrap: [AppComponent]
